e5c686ec4dd568edb4572a161d121435e29a25e9
pedropcg1466 <pedropaulo.c.goulart@gmail.com>
2026-08-27 16:31:01 -0300

    Exibe o endereço de forma legível

--- consulta_cep.py
+++ consulta_cep.py
@@ -4,5 +4,9 @@ cep = "01310930"
 url = f"https://viacep.com.br/ws/{cep}/json/"
 resposta = requests.get(url)
 dados = resposta.json()
-print(dados)
+print("CEP:", dados["cep"])
+print("Rua:", dados["logradouro"])
+print("Bairro:", dados["bairro"])
+print("Cidade:", dados["localidade"])
+print("Estado:", dados["uf"])
 
